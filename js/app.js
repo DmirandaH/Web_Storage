79279e13348cd88c7a771b5cb5ae2d687b274d1f
mirdayan <dmirandaherrera@gmail.com>
2026-02-15 17:51:55 -0500

    Mostrando la lista de tareas en el HTML,validación del formulario, creando las funciones de eliminar, resetear y el método setTimeout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 const formulario = document.querySelector('#formulario');
 const listaTasks = document.querySelector('#lista-tasks');
-const tasks = [];
+let tasks = [];
 
 
 // Event Listeners
@@ -19,5 +19,83 @@ function eventListeners () {
 function agregarTasks(e) {
     e.preventDefault();
 
-    console.log('agregando Tasks............')
+  /// Textarea donde el usurio escribe
+
+  const task = document.querySelector('#task').value;
+
+// validación del formulario
+if(task === '') {
+    mostrarError ('Un nuevo mensaje no puede ir vacio');
+
+    return; // evita que se ejecuten más lineas de código
 }
+
+const taskObj = {
+    id: Date.now(),
+    task:task
+}
+
+// Añadir al arreglo las tasks
+
+tasks=[...tasks, taskObj];
+
+// Una vez agregado vamos a crear el HTML
+crearHTML();
+
+// Reiniciar el formulario();
+formulario.reset();
+
+
+}
+
+// Mostrar Mensaje de error,es una función que se puede reeutilizar en otro proyecto.
+
+function mostrarError (error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertar mensaje de error en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+
+    // Eliminamos la alerta despúes de 3 segundos
+    setTimeout(() => {
+        mensajeError.remove();
+
+    }, 3000);
+
+}
+
+// Muestra un listado de las Tasks
+
+function crearHTML() {
+
+    LimpiarHTML();
+
+    if(tasks.length > 0) {
+        tasks.forEach(task => {
+            // Crear el HTML
+            
+            const li = document.createElement('li');
+
+            //añadir el texto
+            li.innerText = task.task;
+
+            // insertarlo en el html
+            listaTasks.appendChild(li); // Mientras exista un appendChild no se elimina el código previo.
+
+        });
+
+    }
+}
+
+
+// Limpiar el HTML
+function LimpiarHTML() {
+    while(listaTasks.firstChild) {
+        listaTasks.removeChild(listaTasks.firstChild);
+    }
+}
+
